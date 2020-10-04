cdba04c0ef32981476a3e44c64c662e556f0b094
Will Dey <will_dey@college.harvard.edu>
2020-10-04 11:51:52 -0500

    Angelika look at grid.py

---
+++ grid.py
@@ -0,0 +1,15 @@
+import csv
+import ffmpeg
+
+'''
+Put all videos in cwd into 3x2? (use variables for those) grid, with filename (no extension) overlaid in bottom left corner, zoom-style.
+
+https://stackoverflow.com/questions/11552565/vertically-or-horizontally-stack-mosaic-several-videos-using-ffmpeg
+
+https://github.com/kkroening/ffmpeg-python
+'''
+def grid():
+    pass
+
+if __name__ == "__main__":
+    pass
