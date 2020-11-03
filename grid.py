3f70abe127e05c10303e99cc578316c3c6823ec6
AngelikaAntsmae <65563534+AngelikaAntsmae@users.noreply.github.com>
2020-11-03 13:19:22 +0300

    Update grid.py

--- grid.py
+++ grid.py
@@ -38,7 +38,6 @@ import ffmpeg
 
 # TODO add heat#n https://stackoverflow.com/questions/22710099/ffmpeg-create-blank-screen-with-text-video?fbclid=IwAR12ySj7Ck1b3XMIKNZmlhm1-CTAxSKKq1tZhvzR_fQiYEs4m0MXoL1bqlY
 # TODO need to fit all videos to common size https://superuser.com/questions/566998/how-can-i-fit-a-video-to-a-certain-size-but-dont-upscale-it-with-ffmpeg
-
 def main():
     heat(sys.argv[1])
 
@@ -47,28 +46,28 @@ def heat(folder):
     DIR = folder
     # reading in the files, likely change .mov to .mp4 but currently just testing with my .mov files
     files = [name for name in os.listdir(DIR) if name.endswith('.mov') and os.path.isfile(os.path.join(DIR, name))]
-
+    audioFile = [name for name in os.listdir(DIR) if name.endswith('.m4a') and os.path.isfile(os.path.join(DIR, name))]
     print(files)
     # count the number of videos and call grid
     heats = math.ceil(len(files)/4)
     # heats_temp = ffmpeg.input(grid(files[0: 4], 0))
-    heats_temp = (grid(files[0: 4], 0))
+    heats_temp = (grid(files[0: 4], 0, audioFile[0]))
     if heats > 0:
         for i in range(1, heats):
             if i < heats-1:
                 # input files by 4 unless it's the last heat
-                heats_temp = ffmpeg.concat(heats_temp, grid(files[(i*4):(i+1)*4], i))
+                heats_temp = ffmpeg.concat(heats_temp, grid(files[(i*4):(i+1)*4], i, audioFile[0]))
             else:
                 (   
                 ffmpeg
-                .concat(heats_temp, grid(files[i*4::], i))
+                .concat(heats_temp, grid(files[i*4::], i, audioFile[0]))
                 .output('heats.mp4')
                 .run()
                 )    
         
         
 
-def grid(heatFiles, heat):
+def grid(heatFiles, heat, audioFile):
     # if there's 4 files for the heat 
     if len(heatFiles) > 3:
         in0 = ffmpeg.input(heatFiles[0])
@@ -88,7 +87,9 @@ def grid(heatFiles, heat):
         gridText1 = ffmpeg.drawtext(gridText0, text=(heatFiles[1].split('.')[0]), x=1200, y=570, escape_text=True, fontsize = 108, box=1, boxborderw = 24, boxcolor='white')
         gridText2 = ffmpeg.drawtext(gridText1, text=(heatFiles[2].split('.')[0]), x=50, y=1300, escape_text=True, fontsize = 108, box=1, boxborderw = 24, boxcolor='white')
         gridText3 = ffmpeg.drawtext(gridText2, text=(heatFiles[3].split('.')[0]), x=1200, y=1300, escape_text=True, fontsize = 108, box=1, boxborderw = 24, boxcolor='white')
-        return gridText3
+        audioInput = ffmpeg.input(audioFile)
+        gridAudio = ffmpeg.concat(gridText3, audioInput, v=1, a=1)
+        return gridAudio
 
     else:
         # TODOOOOOOOO
